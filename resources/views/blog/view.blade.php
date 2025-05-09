5558160ae3236ccfb88930919f90b1949d977af9
Efrain <carlosefrainchallo1@gmail.com>
2025-05-08 23:49:50 -0500

    Se puede visualizar la imagenes y en ello al dar click, aparece el titulo y la descripción

--- resources/views/blog/view.blade.php
+++ resources/views/blog/view.blade.php
@@ -22,143 +22,50 @@
                         <div class="theiaStickySidebar">
                             <!--Post-single-->
                             <div class="post-single">
+                                <!-- Imagen Principal del Post -->
                                 <div class="post-single__image">
-                                    <img src="assets/img/blog/2.jpg" alt="" class="post-single__image-img">
+                                    <img src="{{ asset('storage/' . $post->featured_image) }}" alt="{{ $post->title }}" class="post-single__image-img">
                                 </div>
 
+                                <!-- Contenido del Post -->
                                 <div class="post-single__content">
-                                    <a href="blog-grid.html" class="category">travel</a>
-                                    <h2 class="post-single__title">What The Secrets You Will know about Jordan Petra if Visit it One Day? </h2>
+                                    <!-- Categorías -->
+                                    @foreach ($post->categories as $category)
+                                        <a href="#" class="category">{{ $category->name }}</a>
+                                    @endforeach
+
+                                    <!-- Título del Post -->
+                                    <h2 class="post-single__title">{{ $post->title }}</h2>
+
+                                    <!-- Meta Información -->
                                     <ul class="post-single__meta list-inline">
                                         <li class="post-single__meta-item">
-                                            <a href="author.html">
-                                                <img src="assets/img/author/1.jpg" alt="" class="post-single__meta-img">
+                                            <a href="#">
+                                                <img src="{{ asset('assets/img/author/admin.png') }}" alt="{{ $post->user->name }}" class="post-single__meta-img">
                                             </a>
                                         </li>
-                                        <li class="post-single__meta-item ">
-                                            <a href="author.html" class="post-single__meta-link">David Smith</a>
+                                        <li class="post-single__meta-item">
+                                            <a href="#" class="post-single__meta-link">{{ $post->user->name }}</a>
                                         </li>
-                                        <li class="post-single__meta-item "> <span class="dot"></span>  February 10, 2022</li>
+                                        <li class="post-single__meta-item">
+                                            <span class="dot"></span> {{ $post->published_at->translatedFormat('d F Y') }}
+                                        </li>
                                     </ul>
                                 </div>
 
+                                <!-- Cuerpo del Post -->
                                 <div class="post-single__body">
-                                    <p>
-                                        Its sometimes her behaviour are contented. Do listening am eagerness oh objection collected. Together gay feelings continue
-                                        juvenile had off Unknown may service
-                                        subject her letters one bed. Child years noise ye in forty. Loud in this in both
-                                        hold. My entrance me is disposal bachelor remember relation
-                                    </p>
-                                    <h5> 1 - Pick a sustainable travel destination </h5>
-
-                                    <p>
-                                        Oh acceptance apartments up sympathize astonished delightful. Waiting him new lasting towards. Continuing melancholy especially
-                                        so to. Me unpleasing  impossible in attachment announcing so astonished. What ask leaf may nor upon door. Tended remain
-                                        my do stairs. Oh smiling amiable am so visited cordial in offices hearted.
-                                    </p>
-                                    <p>
-                                        Oh acceptance apartments up sympathize astonished delightful. Waiting him new lasting towards. Continuing melancholy especially
-                                        so to. Me unpleasing impossible in attachment announcing so astonished. What ask leaf may nor upon door. Tended remain
-                                        my do stairs. Oh smiling amiable am so visited cordial in offices hearted.
-                                    </p>
-                                    <div class="row">
-                                        <div class="col-md-12 ">
-                                            <div class="block-image">
-                                                <img src="assets/img/blog/30.jpg" alt="" class="block-image__img">
-                                                <p class="block-image__caption">Caption can be used to add info</p>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <p> Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-                                        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-                                        in culpa qui officia
-                                        deserunt mollit anim id est laborum.
-                                    </p>
-                                    <h5>2 -  Research before booking</h5>
-
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                        cupidatat non proident.
-                                    </p>
-                                    <div class="quote">
-                                        <i class="bi bi-quote quote__icon"></i>
-                                        <h5 class="quote__text">
-                                            The man who goes alone can start today, but he who
-                                            travels with another must wait till that other is ready.
-                                        </h5>
-                                        <small class="quote__author">Henry David Thoreau.</small>
-                                    </div>
-
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                        irure dolor Unknown may service in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                        cupidatat non proident.
-                                    </p>
-                                    <h5>3 - Pack light , Easy Sustainable Travel Tip</h5>
-
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                        cupidatat non proident.
-                                    >sunt in culpa qui officia deserunt mollit anim id e st laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                                        do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam.
-                                    </p>
-                                    <div class="row ">
-                                        <div class="col-md-6 ">
-                                            <div class="block-image">
-                                                <img src="assets/img/blog/1.jpg" alt="" class="block-image__img">
-                                                <p class="block-image__caption">Caption can be used to add info</p>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6 ">
-                                            <div class="block-image">
-                                                <img src="assets/img/blog/4.jpg" alt="" class="block-image__img">
-                                                <p class="block-image__caption">Caption can be used to add info</p>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                        cupidatat non proident.
-                                    </p>
-                                    <h5>4 - Be respectful of the environment</h5>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-
-                                    </p>
-                                    <ul class="list">
-                                        <li>Be respectful of the environment </li>
-                                        <li>Pick a sustainable travel destination instead of a popular one</li>
-                                        <li>Research before booking</li>
-                                        <li>Pack light , Easy Sustainable Travel Tip</li>
-                                        <li>Be respectful of the environment</li>
-                                    </ul>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                    </p>
+                                    <p>{!! $post->content !!}</p>
                                 </div>
 
+                                <!-- Tags del Post -->
                                 <div class="post-single__footer">
                                     <ul class="list-inline widget__tags">
-                                        <li class="widget__tags-item">
-                                            <a href="blog-grid.html" class="widget__tags-link">Travel</a>
-                                        </li>
-                                        <li class="widget__tags-item">
-                                            <a href="blog-grid.html" class="widget__tags-link">nature</a>
-                                        </li>
-                                        <li class="widget__tags-item">
-                                            <a href="blog-grid.html" class="widget__tags-link">tips</a>
-                                        </li>
-                                        <li class="widget__tags-item">
-                                            <a href="blog-grid.html" class="widget__tags-link">forest</a>
-                                        </li>
-                                        <li class="widget__tags-item">
-                                            <a href="blog-grid.html" class="widget__tags-link">Torism</a>
-                                        </li>
+                                        @foreach ($post->tags as $tag)
+                                            <li class="widget__tags-item">
+                                                <a href="#" class="widget__tags-link">{{ $tag->name }}</a>
+                                            </li>
+                                        @endforeach
                                     </ul>
                                 </div>
                             </div>

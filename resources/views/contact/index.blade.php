24966a12134b087011d711edfd04a2bcc18a0b31
Efrain <carlosefrainchallo1@gmail.com>
2025-05-10 10:29:40 -0500

    Se agrega la interfaz de contact.

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -13,48 +13,52 @@
     <!-- Header -->
     @include('partials.header')
 
-    <main class="main">
-        <!--contact us-->
-        <section class="m-top">
+    <main class="main ">
+        <!--about-us-->
+        <section class="m-top mb-10">
             <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-8 m-auto">
-                        <div class="contact">
-                            <div class="google-map contact__google-map">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3104.5761533072873!2d-78.19644468515456!3d38.91080675375955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b5c5dc680d0b2b%3A0x1e9ff0b6bb7a2f87!2s1000%20Proctor%20Ln%2C%20Front%20Royal%2C%20VA%2022630%2C%20%C3%89tats-Unis!5e0!3m2!1sfr!2sma!4v1578068093888!5m2!1sfr!2sma"
-                                    allowfullscreen=""
-                                    class="contact__google-map-iframe">
-                                </iframe>
+                <div class="row ">
+                    <div class="col-xl-9 m-auto">
+                        <div class="about-us">
+                            <div class="about-us__image">
+                                <img src="assets/img/pic/about-us.jpg" alt="" class="about-us__img">
                             </div>
-                            <form action="assets/php/mail.php" class="widget__form contact__form " method="POST" id="main_contact_form">
-                                <h5 class="widget__form-title">Feel free to contact any time.</h5>
-                                <p class="widget__form-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, repudiandae.</p>
-                                <div class="alert alert-success contact__msg" style="display: none" role="alert">
-                                    Your message was sent successfully.
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" name="name" id="name" class="form-control widget__form-input" placeholder="Your Name*" required="required">
-                                </div>
+                            <div class="about-us__description">
+                                <p class="about-us__description-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus facere, nemo dignissimos, in labore nostrum provident
+                                    perferendis minima accusamus aliquam reprehenderit autem
+                                </p>
+                                <p class="about-us__description-text">
+                                    praesentium sunt beatae libero non totam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus facere, nemo dignissimos, in labore nostrum provident
+                                    perferendis minima accusamus aliquam reprehenderit autem praesentium sunt beatae libero non totam.
+                                </p>
+                                <p class="about-us__description-text">
+                                    praesentium sunt beatae libero non totam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus facere, nemo dignissimos, in labore nostrum provident
+                                    perferendis minima accusamus aliquam reprehenderit autem praesentium sunt beatae libero non totam.
+                                </p>
+                                <div class="about-us__qoute">
+                                    <i class="bi bi-quote about-us__qoute-icon"></i>
 
-                                <div class="form-group">
-                                    <input type="email" name="email" id="email" class="form-control widget__form-input" placeholder="Your Email*" required="required">
+                                    <h3 class="about-us__qoute-item">
+                                        "My website’s kind of fun for me. I get to do drawings on that.
+                                        It’s kind of fun.”
+                                    </h3>
+                                    <small class="about-us__qoute-author">Jeff Bridges.</small>
                                 </div>
-
-                                <div class="form-group">
-                                    <input type="text" name="subject" id="subject" class="form-control widget__form-input" placeholder="Your Subject*" required="required">
-                                </div>
-
-                                <div class="form-group">
-                                    <textarea name="message" id="message" cols="30" rows="5" class="form-control widget__form-input" placeholder="Your Message*" required="required"></textarea>
-                                </div>
-
-                                <button type="submit" name="submit" class="btn-custom">Send Message</button>
-                            </form>
+                            <p class="about-us__description-text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur malesuada malesuada metus ut placerat. Cras a porttitor
+                                quam, eget ornare sapien. In sit amet vulputate metus. Nullam eget rutrum nisl. Sed tincidunt lorem sed maximus interdum.
+                                Interdum malesuada fames ante ipsum primis in faucibus. Aenean scelerisque efficitur mauris nec tincidunt. cursus leo ultricies
+                                magna faucibus id.
+                            </p>
+                            <p class="about-us__description-text">
+                                praesentium sunt beatae libero non totam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus facere, nemo dignissimos, in labore nostrum provident
+                                perferendis minima accusamus aliquam reprehenderit autem praesentium sunt beatae libero non totam.
+                            </p>
+                            </div>
                         </div>
                     </div>
 
                 </div>
-
             </div>
         </section>
 
